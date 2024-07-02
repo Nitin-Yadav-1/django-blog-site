2c70eba93e8ca7a2aaec7167d1607f7b275cf32e
Nitin Yadav <nitinyadav3720@gmail.com>
2024-07-02 18:25:58 +0530

    create profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('signup/', views.signup, name='signup'),
+  path('<int:id>/', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.views.decorators.http import require_http_methods
-from . import forms
+from django.views.decorators.http import require_http_methods, require_GET
+from django.http import Http404
+from . import forms, models
 
 
 @require_http_methods(['GET', 'POST'])
@@ -13,3 +14,12 @@ def signup(request):
   else:
     form = forms.CustomUserCreationForm()
   return render(request, 'registration/signup.html', {'form': form})
+
+
+@require_GET
+def profile(request, id):
+  user = models.CustomUser.objects.get(id=id)
+  if user is None:
+    raise Http404('User does not exist.')
+  return render(request, 'profile.html', {"request_user" : user})
+
