c9b6dff4416989dfb7c5a51277589929c19d43bf
Nitin Yadav <nitinyadav3720@gmail.com>
2024-07-02 17:45:40 +0530

    create signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+from . import forms
 
-# Create your views here.
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+  if request.method == 'POST':
+    form = forms.CustomUserCreationForm(request.POST, request.FILES)
+    if form.is_valid():
+      form.save()
+      return redirect('login')
+  else:
+    form = forms.CustomUserCreationForm()
+  return render(request, 'registration/signup.html', {'form': form})
