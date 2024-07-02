401f3ccb58cea9f7079040607f28ec5df6c60539
Nitin Yadav <nitinyadav3720@gmail.com>
2024-07-02 14:08:38 +0530

    create CustomUser model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+from . import forms
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+  add_form = forms.CustomUserCreationForm
+  form = forms.CustomUserChangeForm
+  model = models.CustomUser
+  list_display = ['username', 'email', 'profile_image_big', 'profile_image_small']
+
+
+admin.site.register(models.CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+  PROFILE_IMAGE_BIG_SIZE = (300,300)
+  PROFILE_IMAGE_SMALL_SIZE = (50,50)
+
+  profile_image_big = models.ImageField(
+    upload_to='profile_image/',
+    default='profile_image/default_big.jpeg',
+  )
+  profile_image_small = models.ImageField(
+    upload_to='profile_image/',
+    default='profile_image/default_small.jpeg'
+  )
+
+  def __str__(self):
+    return self.username
