363558305d27bf59d99835751dc31166f0c532d6
Nitin Yadav <nitinyadav3720@gmail.com>
2024-07-03 08:05:34 +0530

    create all-blogs page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET, require_http_methods
+from . import models
 
-# Create your views here.
+
+@require_GET
+def allBlogs(request):
+  blogs = models.Blog.objects.all()
+  return render(request, 'all_blogs.html', {'blogs' : blogs})
--- blog_site/urls.py
+++ blog_site/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
