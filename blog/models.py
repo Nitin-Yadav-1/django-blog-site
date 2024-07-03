0a66b39760f10faaccb41caea1b191f6d7c59fa1
Nitin Yadav <nitinyadav3720@gmail.com>
2024-07-03 07:45:44 +0530

    create models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Tag(models.Model):
+  name = models.CharField(max_length=30, unique=True)
+
+  def __str__(self):
+    return self.name
+  
+  def save(self, *args, **kwargs):
+    self.name = self.name.lower()
+    return super().save(*args, **kwargs)
+
+
+class Blog(models.Model):
+  title = models.CharField(max_length=200, unique=True)
+  content = models.TextField(max_length=5000)
+  created_at = models.DateField(auto_now_add=True)
+  last_updated_at = models.DateField(auto_now=True)
+  author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  tags = models.ManyToManyField(Tag)
+  likes = models.ManyToManyField(CustomUser, related_name='liked_blogs_set')
+
+  def __str__(self):
+    return self.title[:20] if len(self.title) <= 20 else f"{self.title[:10]}...{self.title[-10:]}"
+
+
+class Comment(models.Model):
+  user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+  content = models.CharField(max_length=500)
+
+
